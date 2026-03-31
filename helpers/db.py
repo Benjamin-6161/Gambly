ad8e34e98d4ed3901bb44f1d58699f17ad9efaf8
Benjamin-6161 <egbojambwaec@gmail.com>
2026-03-31 12:02:37 +0100

    Update db.py

--- helpers/db.py
+++ helpers/db.py
@@ -12,7 +12,7 @@ def get_db_path():
 def save_matches(matches):
   # Connect to database (creates file if it doesn't exist)
   
-  db_path = get_db_path
+  db_path = get_db_path()
 
   conn = sqlite3.connect(db_path)
   conn.row_factory = sqlite3.Row
